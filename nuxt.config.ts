26d302b1a608c11b4d683e7fe519cdb4ea6b49cf
sg_ti004 <sg_ti004@ol.local>
2025-01-21 12:03:31 -0500

    Added nuxtjs tailwind integration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+
   runtimeConfig: {
     public: {
       apiBaseUrl: 'http://localhost:8055/items/',
     }
-  }
+  },
+
+  modules: ['@nuxtjs/tailwindcss']
 })
